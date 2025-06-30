f334a1c7de603cbe786d0b503b3fd576d488dc74
ailton9017 <137803791+ailton9017@users.noreply.github.com>
2025-06-30 10:46:27 -0300

    aula 9

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const botoes = document.querySelectorAll(".botoes");
+const textos = document.querySelectorAll(".abas-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo01 = new Date ("2025-07-09");
+const tempoAtual = new Date ();
 console.log(botoes)
 
 for(let i=0; i< botoes.length; i++){
@@ -6,7 +10,17 @@ for(let i=0; i< botoes.length; i++){
         
         for (let j=0; j<botoes.length; j++){
             botoes[j].remove.add("ativo");
+            textos[j].remove.add("ativo");
         }
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+
+function calculadoraTempo{
+      let tempoAtual = new Date();
+      let tempoFinal = tempoObjetivo01-tempoAtual;
+      let segundos = Math.floor (tempoFinal)1000x
+}
+contadores[0]. textContent = "Contagem Regressiva"
+contadores[0]. textContent = calculadoraTempo (tempoObjetivo01)
